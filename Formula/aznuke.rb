39163bfb5aaf4947d33fb37f25c2d0f4ef49aeb6
Sammie <thesamokorie@gmail.com>
2025-07-04 15:12:06 +0000

    Update aznuke to v0.1.4 with correct SHA256

--- Formula/aznuke.rb
+++ Formula/aznuke.rb
@@ -1,8 +1,8 @@
 class Aznuke < Formula
   desc "A powerful CLI tool for scanning and cleaning up Azure resources"
   homepage "https://github.com/sojay/azure-nuke"
-  url "https://github.com/sojay/azure-nuke/archive/v0.1.3.tar.gz"
-  sha256 "PLACEHOLDER_SHA256"
+  url "https://github.com/sojay/azure-nuke/archive/v0.1.4.tar.gz"
+  sha256 "723fea96287d3d97794f3ed1633df4d35ce689f1d20c011b22c8a90d1a81495c"
   license "MIT"
 
   depends_on "python@3.11"
