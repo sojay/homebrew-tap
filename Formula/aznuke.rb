e3c25eb8154b65ecb52c661cbe85bf2843aef5a7
Sammie <thesamokorie@gmail.com>
2025-07-04 15:36:44 +0000

    Fix dependency installation by using proper pip install method

--- Formula/aznuke.rb
+++ Formula/aznuke.rb
@@ -10,7 +10,9 @@ class Aznuke < Formula
   depends_on "python@3.11"
 
   def install
-    virtualenv_install_with_resources
+    venv = virtualenv_create(libexec, "python3.11")
+    system libexec/"bin/pip", "install", "-v", "--ignore-installed", buildpath
+    bin.install_symlink libexec/"bin/aznuke"
   end
 
   test do
